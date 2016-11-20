535f31da553e6644d3cfbab0ff934e42a0649ecf
Kotyara <krivonos-artem-alexandrovich@yandex.ru>
2016-11-20 16:33:02 +0300

    feat(app): add User model

--- models.py
+++ models.py
@@ -1,7 +1,13 @@
 # -*- coding: UTF-8 -*-
 from mongoengine import Document, fields
+from flask_login import UserMixin
 
 
 class Message(Document):
     user = fields.StringField(required=True)
     message = fields.StringField(required=True)
+
+
+class User(Document, UserMixin):
+    name = fields.StringField(required=True)
+    password = fields.StringField(required=True)
