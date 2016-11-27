841d773f91c2892f9a8ebeab6eb28bb10ad05c36
Kotyara <krivonos-artem-alexandrovich@yandex.ru>
2016-11-27 23:42:29 +0300

    feat(angular-app): add satellizer and login

--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,52 @@
 console.log('app.js');
 
-angular.module('chatApp', ['ui.router'])
-    .config(function($stateProvider) {
-      var loginState = {
-        name: 'login',
-        url: '/login',
-        templateUrl: '/static/html/login.html',
-        controller: 'LoginController'
-      }
+angular.module('chatApp', ['ui.router', 'satellizer'])
+    .config(function($stateProvider, $authProvider, $qProvider) {
+        var loginState = {
+            name: 'login',
+            url: '/login',
+            templateUrl: '/static/html/login.html',
+            controller: 'LoginController'
+        }
 
-      var chatState = {
-        name: 'chat',
-        url: '/chat',
-        templateUrl: '/static/html/chat.html',
-        controller: 'ChatController'
-      }
+        var chatState = {
+            name: 'chat',
+            url: '/chat',
+            templateUrl: '/static/html/chat.html',
+            controller: 'ChatController'
+        }
 
-      $stateProvider.state(loginState);
-      $stateProvider.state(chatState);
+        $stateProvider.state(loginState);
+        $stateProvider.state(chatState);
+
+
+        $authProvider.baseUrl = '/';
+        $authProvider.loginUrl = '/login';
+        $authProvider.signupUrl = '/registration';
+        $authProvider.unlinkUrl = '/auth/unlink/';
+        $authProvider.tokenName = 'token';
+        $authProvider.tokenPrefix = 'satellizer';
+        $authProvider.tokenHeader = 'Authorization';
+        $authProvider.tokenType = 'JWT';
+        $authProvider.storageType = 'localStorage';
+
+        $qProvider.errorOnUnhandledRejections(false);
     })
     .run(['$state', function($state){
-      $state.go('login')
+        $state.go('login')
     }])
-    .controller('LoginController', function() {
+    .controller('LoginController', function($scope, $auth, $state) {
+        $scope.login = function() {
+            console.log($scope.user)
+            $auth.login($scope.user)
+                .then(function() {
+                    console.log('You have successfully signed in!');
+                    $state.go('chat')
+                })
+                .catch(function(error) {
+                    console.warn(error.data.message, error.status);
+                });
+        };
     })
     .controller('ChatController', function() {
         var self = this;
@@ -51,6 +75,6 @@ angular.module('chatApp', ['ui.router'])
         };
     });
 
-//  TODO routing
+// TODO register
 //  TODO login
 //  TODO logout
