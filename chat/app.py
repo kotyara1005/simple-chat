162d2c647ad1b2ebc3825ce221d7f8d6584a6aaa
Artem Krivonos <ak@zvooq.com>
2018-09-18 15:47:38 +0300

    Add create_app function

--- chat/app.py
+++ chat/app.py
@@ -7,21 +7,23 @@ from chat.utils import ApiError, handle_error
 from config import config
 
 
-# TODO use create app
-app = Flask(__name__, static_folder=config.STATIC_FOLDER)
-app.config.from_object(config)
-app.debug = config.DEBUG
-app.errorhandler(ApiError)(handle_error)
-app.register_blueprint(auth.create_blueprint(), url_prefix='/api')
-app.before_request(auth.setup_user)
-app.register_blueprint(messages.create_blueprint(), url_prefix='/api')
-app.register_blueprint(front.bp, url_prefix='')
-#TODO move to postgres
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db.init_app(app)
+def create_app():
+    app = Flask(__name__, static_folder=config.STATIC_FOLDER)
+    app.config.from_object(config)
+    app.debug = config.DEBUG
+    app.errorhandler(ApiError)(handle_error)
+    app.register_blueprint(auth.create_blueprint(), url_prefix='/api')
+    app.before_request(auth.setup_user)
+    app.register_blueprint(messages.create_blueprint(), url_prefix='/api')
+    app.register_blueprint(front.bp, url_prefix='')
+    #TODO move to postgres
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+    db.init_app(app)
+    return app
 
 
 if __name__ == '__main__':
+    app = create_app()
     # db.drop_all(app=app)
     db.create_all(app=app)
     app.run(debug=True)
@@ -30,3 +32,4 @@ if __name__ == '__main__':
 # TODO docker
 # TODO add participant
 # TODO create chat
+# TODO add logout
